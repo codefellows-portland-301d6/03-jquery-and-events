72cb2c4db062e90d527401e4498da72dd4fcd033
Andrew Beers <andybeers@gmail.com>
2016-09-21 14:53:36 -0700

    Hid template from DOM on select menu reset.

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -36,6 +36,7 @@ articleView.handleAuthorFilter = function() {
       /* TODO: Otherwise, we should:
       1. Show all the articles.
       2. Except the one article we are using as a template. */
+      $('section article').not('.template').show();
     }
     $('#category-filter').val('');
   });
