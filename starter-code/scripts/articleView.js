23ccf696d41857f711c64c3414e847bc8e61ca8d
Tyler Smith <tyler.smith6891@gmail.com>
2016-09-21 17:37:29 -0700

    finished!!!!!!!

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -108,6 +108,11 @@ articleView.setTeasers = function() {
   2. Reveal everything in that particular article now.
   3. Hide that read-on link! */
   // STRETCH GOAL!:  change the 'Read On' link to display 'Show Less'
+  $('.read-on').on('click', function(event) {
+    event.preventDefault();
+    $(this).parent().find('.article-body *').fadeIn();
+    $(this).hide();
+  });
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
